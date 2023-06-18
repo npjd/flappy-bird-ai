226f9bd3020a30a3f3d5718aaead2bf9dec8d966
Nima Pourjafar <nima.pourjafar123@gmail.com>
2023-06-18 11:12:59 -0400

    commetn

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -1,15 +1,27 @@
+// Name: Nima Pourjafar
+// Date: June 18th 2023
+// Description: This is flappy AI, a game where you can train an AI to play flappy bird.
+
 package Game;
 
 import javax.swing.JFrame;
 
+// Main class that runs the game
 public class Game {
     
-
+    /*
+     * Main method that runs the game.
+     * 
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
+        // create a new JFrame window
         JFrame window = new JFrame("Flappy AI");
 
+        // set the content pane to be the game panel
         window.setContentPane(new GamePanel());
-        
+    
+        // set the window to be visible
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setResizable(false);
 		window.pack();
--- src/Game/GamePanel.java
+++ src/Game/GamePanel.java
@@ -7,110 +7,130 @@ import java.awt.event.*;
 import javax.swing.JPanel;
 
 import GameState.GameStateManager;
+
 // JPanel that the game will run on
 // Make GamePanel extend JPanel and implement runnable & key listener
-public class GamePanel extends JPanel 
-	implements Runnable, KeyListener{
+public class GamePanel extends JPanel
+		implements Runnable, KeyListener {
 	// variables for dimensions
 	public static final int WIDTH = 500;
 	public static final int HEIGHT = 800;
 	public static final int SCALE = 1;
-	
+
 	// variables for game thread
 	private Thread thread;
 	private boolean running;
 	private int FPS = 60;
 	private long targetTime = 1000 / FPS;
-	
+
 	// variables for drawing
 	private BufferedImage image;
 	private Graphics2D g;
-	
+
 	// game's game state manager
 	private GameStateManager gsm;
-	
+
+	/*
+	 * GamePanel constructor
+	 */
 	public GamePanel() {
 		// Call JPanel super funciton
 		super();
 		// set the dimensions of our JPanel
 		setPreferredSize(
-			new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
-		// allows for user to select (or focus) the game window to use keyboard 
+				new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+		// allows for user to select (or focus) the game window to use keyboard
 		setFocusable(true);
 	}
-	
+
+	/*
+	 * Function to start the thread and key listener
+	 */
 	public void addNotify() {
 		super.addNotify();
-		// start the thread and key listener 
-		if(thread == null) {
+		// start the thread and key listener
+		if (thread == null) {
 			thread = new Thread(this);
 			addKeyListener(this);
 			thread.start();
 		}
 	}
-	
+
+	/*
+	 * Function to initialize the game panel
+	 */
 	private void init() {
 		// create image that hosts all the graphics on the panel
 		image = new BufferedImage(
-					WIDTH, HEIGHT,
-					BufferedImage.TYPE_INT_RGB
-				);
+				WIDTH, HEIGHT,
+				BufferedImage.TYPE_INT_RGB);
 		g = (Graphics2D) image.getGraphics();
-		
+
 		running = true;
 		// create game state manager
 		gsm = new GameStateManager(this);
-		
+
 	}
-	// run function for game panel
+
+	/*
+	 * Function to run the game loop and thread
+	 */
 	public void run() {
-		
+
 		init();
-		
+
 		long start;
 		long elapsed;
 		long wait;
-		
+
 		// game loop
-		while(running) {
-			
+		while (running) {
+
 			start = System.nanoTime();
-			
+
 			// call gsm methods
 			draw();
 			update();
 			// draw image to JPanel
 			drawToScreen();
-			
+
 			// calculate the elapsed time since the start
 			elapsed = System.nanoTime() - start;
-			
+
 			// calculate how long to sleep the thread
 			wait = targetTime - elapsed / 1000000;
 			// make sure timeout value is not negative
-			if(wait < 0){
+			if (wait < 0) {
 				wait = 5;
 			}
 			// sleep thread to follow FPS
 			try {
 				Thread.sleep(wait);
-			}
-			catch(Exception e) {
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
-			
+
 		}
-		
+
 	}
-	// method to update the state
+
+	/*
+	 * Function to update the game state manager
+	 */
 	private void update() {
 		gsm.update();
 	}
-	// method to draw state to image
+
+	/*
+	 * Function to draw the game state manager
+	 */
 	private void draw() {
 		gsm.draw(g);
 	}
-	// method to draw image to screen
+
+	/*
+	 * Function to draw the image to the JPanel
+	 */
 	private void drawToScreen() {
 
 		Graphics g2 = getGraphics();
@@ -120,26 +140,39 @@ public class GamePanel extends JPanel
 		g2.dispose();
 	}
 
+	/*
+	 * Function to set the FPS
+	 * 
+	 * @param fps the FPS to set
+	 *
+	 */
 	public void setFPS(int fps) {
 		FPS = fps;
 		targetTime = 1000 / FPS;
 	}
 
+	/*
+	 * Function to get the FPS
+	 * 
+	 * @return the FPS
+	 *
+	 */
 	public int getFPS() {
 		return FPS;
 	}
-	
+
 	// implements key press methods
-	// all key events get passed to the game state manager in order to see how the current game state handles key events
-	public void keyTyped(KeyEvent key) {}
+	// all key events get passed to the game state manager in order to see how the
+	// current game state handles key events
+	public void keyTyped(KeyEvent key) {
+	}
+
 	public void keyPressed(KeyEvent key) {
 		gsm.keyPressed(key.getKeyCode());
 	}
+
 	public void keyReleased(KeyEvent key) {
 		gsm.keyReleased(key.getKeyCode());
 	}
 
 }
-
-
-
